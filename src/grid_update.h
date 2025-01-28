61d0aa56feb6bc5f3b359b5f6da8891bc81a9395
Tony Wang <towang@student.42heilbronn.de>
2025-01-28 21:12:12 +0100

    fixing norm

--- src/grid_update.h
+++ src/grid_update.h
@@ -6,7 +6,7 @@
 /*   By: towang <towang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 11:55:53 by towang            #+#    #+#             */
-/*   Updated: 2025/01/28 19:22:41 by towang           ###   ########.fr       */
+/*   Updated: 2025/01/28 21:11:08 by towang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int		try_grid_val(t_puzzle *grid, int cell_idx, int val);
 void	set_grid_val(t_puzzle *grid, int cell_idx, int val);
 void	unset_grid_val(t_puzzle *grid, int cell_idx);
 int		check_constraints(t_puzzle *puzzle, int insert_idx);
-void	set_active_constraint(t_puzzle *puzzle, int constr_idx);
 
 #endif
