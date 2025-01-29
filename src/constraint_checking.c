fee344e6e8fe1471e53f80a24c9b7d078ea735ec
Tony Wang <towang@student.42heilbronn.de>
2025-01-29 20:54:12 +0100

    seems to be working

--- src/constraint_checking.c
+++ src/constraint_checking.c
@@ -6,7 +6,7 @@
 /*   By: towang <towang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:31:51 by towang            #+#    #+#             */
-/*   Updated: 2025/01/29 19:39:30 by towang           ###   ########.fr       */
+/*   Updated: 2025/01/29 20:53:58 by towang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,24 @@ int	check_active_constr(t_puzzle *puzzle)
 	int					sub_idx;
 	int					grid_idx;
 	int					size;
+	int					continue_loop;
 	t_constraint_state	*constr;
 
 	constr = &puzzle->constr_state;
 	size = constr->size;
 	sub_idx = 0;
-	while (sub_idx < size)
+	continue_loop = 1;
+	while (continue_loop && sub_idx < size)
 	{
 		if (constr->is_reverse)
 			grid_idx = constr->cur_c_pair.grid_indeces[size - sub_idx - 1];
 		else
 			grid_idx = constr->cur_c_pair.grid_indeces[sub_idx];
 		sub_idx++;
-		if (!update_constr_state(puzzle, grid_idx))
-			return (1);
-		if (constr->fwd_lb > constr->cur_c_pair.fwd_val)
-			return (0);
-		if (constr->fwd_ub < constr->cur_c_pair.fwd_val)
-			return (0);
-		if (constr->bwd_ub < constr->cur_c_pair.bwd_val)
+		continue_loop = update_constr_state(puzzle, grid_idx);
+		if (constr->fwd_lb > constr->cur_c_pair.fwd_val
+			|| constr->fwd_ub < constr->cur_c_pair.fwd_val
+			|| constr->bwd_ub < constr->cur_c_pair.bwd_val)
 			return (0);
 	}
 	return (1);
@@ -52,32 +51,28 @@ int	update_constr_state(t_puzzle *puzzle, int grid_idx)
 
 	constr = &puzzle->constr_state;
 	new_val = puzzle->grid_vals[grid_idx];
-	if (new_val == puzzle->size)
-		return (0);
 	if (new_val == 0)
 	{
 		if (constr->max_height_ub == puzzle->size)
 			return (1);
 		find_cell_bounds(puzzle, grid_idx, &new_val_lb, &new_val_ub);
 		update_constr_bounds_unset(constr, new_val_lb, new_val_ub);
+		return (1);
 	}
 	else
 	{
-		update_constr_bounds_new_val(constr, new_val);
+		return (update_constr_bounds_new_val(constr, new_val));
 	}
-	return (1);
 }
 
 void	update_constr_bounds_unset(t_constraint_state *constr, int lb, int ub)
 {
-	constr->max_height_ub = constr->size;
-	constr->max_height_lb = constr->size;
-	return;
 	if (lb > constr->max_height_ub)
 	{
-		constr->fwd_lb++;
 		constr->max_height_lb = lb;
 		constr->max_height_ub = ub;
+		if (constr->max_height_ub < constr->size)
+			constr->fwd_lb++;
 	}
 	else
 	{
@@ -88,19 +83,25 @@ void	update_constr_bounds_unset(t_constraint_state *constr, int lb, int ub)
 	}
 }
 
-void	update_constr_bounds_new_val(t_constraint_state	*constr, int new_val)
+int	update_constr_bounds_new_val(t_constraint_state	*constr, int new_val)
 {
+	if (new_val == constr->size)
+	{
+		constr->fwd_ub -= new_val - constr->max_height_ub - 1;
+		return (0);
+	}
 	if (new_val > constr->max_height_lb)
 	{
 		constr->max_height_lb = new_val;
 		if (new_val > constr->max_height_ub)
 		{
-			constr->max_height_ub = new_val;
 			constr->bwd_ub--;
 			constr->fwd_ub -= new_val - constr->max_height_ub - 1;
 			constr->fwd_lb++;
+			constr->max_height_ub = new_val;
 		}
 	}
+	return (1);
 }
 
 void	find_cell_bounds(t_puzzle *puzzle, int cell_idx, int *lb, int *ub)
--- src/constraint_checking.h
+++ src/constraint_checking.h
@@ -6,7 +6,7 @@
 /*   By: towang <towang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:31:47 by towang            #+#    #+#             */
-/*   Updated: 2025/01/29 19:14:38 by towang           ###   ########.fr       */
+/*   Updated: 2025/01/29 20:52:24 by towang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int		check_active_constr(t_puzzle *puzzle);
 int		update_constr_state(t_puzzle *puzzle, int grid_idx);
-void	update_constr_bounds_new_val(t_constraint_state	*constr, int new_val);
+int		update_constr_bounds_new_val(t_constraint_state	*constr, int new_val);
 void	find_cell_bounds(t_puzzle *puzzle, int cell_idx, int *lb, int *ub);
 void	update_constr_bounds_unset(t_constraint_state *constr, int lb, int ub);
 
